db08ac7b14a10bd6f6cb323f2bba92da01bc9e27
Elise l <elisedevops@gmail.com>
2026-05-07 15:36:35 +0200

    Update error handling

--- Domain/ErrorViewModel.cs
+++ Domain/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1.Models;
+namespace PortfolioFe.Domain;
 
 public class ErrorViewModel
 {
--- Presentation/Controllers/Errors/ErrorController.cs
+++ Presentation/Controllers/Errors/ErrorController.cs
@@ -24,14 +24,35 @@ public class ErrorController : Controller
                 return View("~/Presentation/Views/Errors/Unauthorized.cshtml");
 
             case HttpRequestException:
-                Response.StatusCode = 500;
+                ViewBag.StatusCode = 404;
                 return View("~/Presentation/Views/Errors/Error.cshtml");
 
             default:
-                Response.StatusCode = 500;
+                ViewBag.StatusCode = 500;
                 return View("~/Presentation/Views/Errors/Error.cshtml");
         }
 
-        ;
+
+    }
+    [Route("Error/StatusCode")]
+    public IActionResult StatusCode(int code)
+    {
+        if (code == 404)
+        {
+            return View("~/Presentation/Views/Errors/NotFound.cshtml");
+        }
+
+        ViewBag.StatusCode = code;
+        return View("~/Presentation/Views/Errors/Error.cshtml");
+    }
+
+    [Route("Error/ServerError")]
+    public IActionResult ServerError()
+    {
+        var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+        // Optional: log exceptionFeature?.Error here
+
+        return View("~/Presentation/Views/Errors/Error.cshtml");
     }
 }
